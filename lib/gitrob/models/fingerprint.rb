7bae8e2840111067fbb42d1f33471ce8c5fd8bd8
Mustaqiim Muhar <mmuhar@zendesk.com>
2017-07-11 19:13:14 +0800

    Add comment as required field

--- lib/gitrob/models/fingerprint.rb
+++ lib/gitrob/models/fingerprint.rb
@@ -2,14 +2,14 @@
 module Gitrob
   module Models
     class FalsePositive < Sequel::Model
-      set_allowed_columns :fingerprint, :path, :repository
+      set_allowed_columns :fingerprint, :path, :repository, :comment
       
       SHA_REGEX = /[a-f0-9]{64}/
       
       def validate
         super
         validates_unique(:fingerprint)
-        validates_presence [:fingerprint, :path, :repository]
+        validates_presence [:fingerprint, :path, :repository, :comment]
         validates_format SHA_REGEX, :fingerprint
       end
     end
